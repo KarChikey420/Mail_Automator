d6056a5da72dc4701ea7fe89d4909dc73f1148c7
KarChikey <kartikeynegi2002@gmail.com>
2025-11-23 12:06:01 +0530

    add

--- app/rag_engine.py
+++ app/rag_engine.py
@@ -1,12 +1,8 @@
 import chromadb
 from email_reader import unread_email_fetcher
-import google.generativeai as genai
 from chromadb.config import Settings
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-genai.configure(api_key=os.getenv('GOOGLE_API'))
 chroma_client=chromadb.Client(Settings(persitst_db="email_rag_db"))
 collection=chroma_client.get_or_create_collection("emails")
 
@@ -17,7 +13,6 @@ def store_emails_in_vector_db(emails):
             metadatas=[{"sender":mail["Sender"],"subject":mail["Subject"]}],
             ids=[f"email_{i}"]
         )
-
-def generate_response(query):
+    print(f"Stored {len(emails)} emails in vector database")
     
     
