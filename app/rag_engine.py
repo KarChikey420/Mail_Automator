c17448e60ca87abe3e259303fbe67f18064d9051
KarChikey <kartikeynegi2002@gmail.com>
2025-11-23 17:09:20 +0530

    add

--- app/rag_engine.py
+++ app/rag_engine.py
@@ -8,36 +8,43 @@ load_dotenv()
 genai.configure(api_key=os.getenv('GOOGLE_API'))
 
 def generte_response(email_data):
-    
-    if spam_or_not(email_data['Body']):
+
+    if spam_or_not(email_data["Body"]):
         print(f"Email from {email_data['Sender']} classified as Spam. No response generated.")
-        return None,"spam"
+        return None, "spam"
     
-    tone_instruction="Use a professional and polite tone."
-    query=(
+    tone_instruction = "Use a professional and polite tone."
+
+    query = (
         f"{tone_instruction}\n"
-        f"generate a consise and professional reply.\n"
-        f"Don't metion AI.\n"
-        f"Email Subject:{email_data['Sender']}\n"
-        f"Subject:{email_data['Subject']}\n"
+        f"Generate a concise and professional reply.\n"
+        f"Do not mention AI.\n"
+        f"Email From: {email_data['Sender']}\n"
+        f"Email Subject: {email_data['Subject']}\n"
         f"Body:\n{email_data['Body']}\n"
     )
-    result=genai.GenerativeModel("gemini-1.5-flash").generate_content(query)
-    context = result.text if result else "No related past emails."
-    
-    model=genai.GenerativeModel("gemini-1.5-flash")
-    final_prompt=""""
+
+    model = genai.GenerativeModel("gemini-1.5-flash")
+
+    result = model.generate_content(query)
+    context = result.text if result else "No relevant past email context."
+
+    final_prompt = f"""
         You are an email assistant.
-        ### Past Email Context:
+
+        ### Context:
         {context}
-        
-        ### Past Details:
+
+        ### Current Email Details:
         {query}
-      
-        Write the reply in proper email format.
-        Ensure the tone is polite, concise, and professional.
-        Do NOT mention that you are an AI.
+
+        Now write the final email reply in proper professional format.
+        Ensure:
+        - Tone is polite and professional
+        - Reply is concise
+        - Do NOT mention that you are an AI
+        - Add a polite closing line
         """
-    
-    response=model.generate_content(final_prompt)
-    return response.text.strip(),"not_spam"
+
+    response = model.generate_content(final_prompt)
+    return response.text.strip(), "not_spam"
