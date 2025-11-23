887b1950343b4437ec0e1ee7e3c75083bd1fa1cc
KarChikey <kartikeynegi2002@gmail.com>
2025-11-23 17:22:56 +0530

    add

--- app/test_email.py
+++
@@ -1,8 +0,0 @@
-from emial_sender import send_email
-from email_reader import unread_email_fetcher
-
-send_email("kartikeynegi2002@gmail.com", "Test Subject", "This is a test email body.")
-
-emails = unread_email_fetcher()
-print(emails)
-
